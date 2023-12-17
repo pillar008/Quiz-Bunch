b596e1642fe203cc73e63ae3d9c4ae0bfbb27e7e
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2023-12-17 22:46:17 +0530

    Update index.js
    22:46
    17 Dec 2023

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizeSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -19,4 +20,23 @@ continueBtn.onclick = () => {
     quizeSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
+
+//    showQuestions(0);
 }
+
+// let questionCount = 0;
+
+// const nextBtn = document.querySelector('.next-btn')
+
+// nextBtn.onclick = () => {
+//     questionCount++;
+//     showQuestions(questionCount);
+// }
+
+// //getting questions and options from array
+
+// function showQuestions(index) {
+//     const questionText = document.querySelector('.question-text');
+//     questionText.textContent = '${questions[index].numb}. ${questions[index].question}';
+// }
