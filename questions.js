9e35ca54c59d1a9752b0284c4f2b29bf2e5de8b0
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2023-12-17 22:48:01 +0530

    Create questions.js
    22:48
    17 Dec 2023

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What doees HTML Stands For?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D, Home Text Multi Language",
+        ]
+    },
+    {
+        numb: 2,
+        question: "The full form of CSS is",
+        answer: "C. Cascading style sheets",
+        options: [
+            "A. Cascade style sheets",
+            "B. Color and style sheets",
+            "C. Cascading style sheets",
+            "D. None of the above",
+        ]
+    },
+    {
+        numb: 3,
+        question: "The property in CSS used to change the background color of an element is",
+        answer: "B. background-color",
+        options: [
+            "A. bgcolor",
+            "B. background-color",
+            "C. color",
+            "D. All of the above",
+        ]
+    },
+    {
+        numb: 4,
+        question: "The HTML attribute used to define the internal stylesheet is",
+        answer: "A. <style>",
+        options: [
+            "A. <style>",
+            "B. style",
+            "C. <link>",
+            "D. <script>",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which type of JavaScript language is",
+        answer: "A. Object-Oriented",
+        options: [
+            "A. Object-Oriented",
+            "B. Object-Based",
+            "C. Assembly-language",
+            "D. High-level",
+        ]
+    },
+];
