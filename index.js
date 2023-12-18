5f6ee10bd522b8370b3db2a15567de5a8b95f89e
Saumya Shah <144374729+SaumyaShah0@users.noreply.github.com>
2023-12-18 23:56:53 +0530

    Update index.js
    23:57
    18 Dec 2023

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
-const quizeSection = document.querySelector('.quiz-section');
+const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
 
 startBtn.onclick = () => {
@@ -17,26 +17,72 @@ exitBtn.onclick = () => {
 }
 
 continueBtn.onclick = () => {
-    quizeSection.classList.add('active');
+    quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
     quizBox.classList.add('active');
 
-//    showQuestions(0);
+    showQuestions(0);
+    questionCounter(1);
 }
 
-// let questionCount = 0;
+let questionCount = 0;
+let questionNumb = 1;
 
-// const nextBtn = document.querySelector('.next-btn')
+const optionList = document.querySelector('.option-list');
 
-// nextBtn.onclick = () => {
-//     questionCount++;
-//     showQuestions(questionCount);
-// }
+const nextBtn = document.querySelector('.next-btn');
 
-// //getting questions and options from array
+nextBtn.onclick = () => {
+    if (questionCount < questions.length -1){
+        questionCount++;
+        showQuestions(questionCount);
 
-// function showQuestions(index) {
-//     const questionText = document.querySelector('.question-text');
-//     questionText.textContent = '${questions[index].numb}. ${questions[index].question}';
-// }
+        questionNumb++;
+        questionCounter(questionNumb);
+    }
+    else{
+        console.log('Question Completed');
+    }
+}
+
+//getting questions and options from array
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for (let i = 0; i < option.length; i++){
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+}
+
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+
+    if (userAnswer == correctAnswer){
+        answer.classList.add('correct');
+    }
+    else {
+        answer.classList.add('incorrect');
+    }
+
+    //if user selected, disavled all optiions
+    for (let i = 0; i<allOptions; i++){
+        optionList.children[i].classList.add('disabled');
+    }
+}
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`
+}
