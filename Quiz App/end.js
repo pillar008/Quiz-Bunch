b4d04ec3d40b97b20a7dbafdba737d833669575e
Saumya <saumyashah516@gmail.com>
2024-02-03 13:55:21 +0530

    upto video 9

--- Quiz App/end.js
+++ Quiz App/end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -10,6 +14,18 @@ username.addEventListener("keyup", () => {
 });
 
 saveHighScore = e => {
-    console.log("click save");
     e.preventDefault();
+
+    const score = {
+        // score: mostRecentScore,
+        score: Math.floor(Math.random()*100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    window.location.assign("/");
 };
